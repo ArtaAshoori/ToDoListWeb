b72283b03a3bd26b55594cb6fed818c8c9e8ebac
Arta <>
2024-06-15 12:25:31 +0330

    init project

---
+++ main.js
@@ -0,0 +1,84 @@
+const form = document.querySelector("#new-task-form");
+const input = document.querySelector("#new-task-input");
+const list_element = document.querySelector("#tasks");
+const btn_remove_all = document.querySelector('#remove-all');
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const task = input.value;
+
+  if (!task) {
+    alert("Please fill out the task");
+    return;
+  }
+
+  const task_element = document.createElement("div");
+  task_element.classList.add("task");
+
+  const task_content_element = document.createElement("div");
+  task_content_element.classList.add("content");
+
+  task_element.appendChild(task_content_element);
+
+  const task_input_element = document.createElement("input");
+  task_input_element.classList.add("text");
+  task_input_element.type = "text";
+  task_input_element.value = task;
+  task_input_element.setAttribute("readonly", "readonly");
+
+  task_content_element.appendChild(task_input_element);
+
+  const task_actions_element = document.createElement("div");
+  task_actions_element.classList.add("actions");
+
+  const task_success_element = document.createElement("button");
+  task_success_element.classList.add("success");
+  task_success_element.innerHTML = "Success";
+
+  const task_delete_element = document.createElement("button");
+  task_delete_element.classList.add("delete");
+  task_delete_element.innerHTML = "Delete";
+
+  const task_edit_element = document.createElement("button");
+  task_edit_element.classList.add("edit");
+  task_edit_element.innerHTML = "Edit";
+
+  task_actions_element.appendChild(task_edit_element);
+  task_actions_element.appendChild(task_delete_element);
+  task_actions_element.appendChild(task_success_element);
+
+  task_element.appendChild(task_actions_element);
+
+  list_element.appendChild(task_element);
+
+  input.value = "";
+
+  task_success_element.addEventListener("click", () => {
+    task_element.style.backgroundColor = "green";
+  });
+
+  task_delete_element.addEventListener("click", () => {
+    list_element.removeChild(task_element);
+  });
+
+  task_edit_element.addEventListener("click", () => {
+    if (task_edit_element.innerHTML == "Edit") {
+      task_input_element.removeAttribute("readonly");
+      task_edit_element.innerHTML = "Save";
+    } else {
+      task_input_element.setAttribute("readonly", "readonly");
+      task_edit_element.innerHTML = "Edit";
+    }
+  });
+});
+
+btn_remove_all.addEventListener("click", () => {
+  while (list_element.firstChild) {
+    list_element.removeChild(list_element.firstChild);
+  }
+});
+
+// function input = meghdar input o bgire
+// event braye input ha onchange
+// daynamic
