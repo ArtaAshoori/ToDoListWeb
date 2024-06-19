82b309751b5c4a29e9b7805e616f6e325dd5275f
Arta <>
2024-06-19 20:06:16 +0330

    Clean code for createElement

--- main.js
+++ main.js
@@ -9,39 +9,28 @@ const btn_filter_success = document.querySelector('#filter-success')
 const btn_filter_not_success = document.querySelector('#filter-not-success')
 
 const createTaskElement = (task) => {
-  const task_element = document.createElement("div");
-  task_element.classList.add("task");
+  const taskElementHtml = `
+  <div class="task">
+      <div class="content">
+        <input type="text" class="text" value="${task}" readonly="readonly" />
+      </div>
+      <div class="actions">
+        <button class="success">Success</button>
+        <button class="delete">Delete</button>
+        <button class="edit">Edit</button>
+      </div>
+    </div>
+  `;
 
-  const task_content_element = document.createElement("div");
-  task_content_element.classList.add("content");
-  task_element.appendChild(task_content_element);
+  const container = document.createElement("div");
+  container.innerHTML = taskElementHtml;
 
-  const task_input_element = document.createElement("input");
-  task_input_element.classList.add("text");
-  task_input_element.type = "text";
-  task_input_element.value = task;
-  task_input_element.setAttribute("readonly", "readonly");
-  task_content_element.appendChild(task_input_element);
+  const task_element = container.firstElementChild;
 
-  const task_actions_element = document.createElement("div");
-  task_actions_element.classList.add("actions");
-
-  const task_success_element = document.createElement("button");
-  task_success_element.classList.add("success");
-  task_success_element.innerHTML = "Success";
-  task_actions_element.appendChild(task_success_element);
-
-  const task_delete_element = document.createElement("button");
-  task_delete_element.classList.add("delete");
-  task_delete_element.innerHTML = "Delete";
-  task_actions_element.appendChild(task_delete_element);
-
-  const task_edit_element = document.createElement("button");
-  task_edit_element.classList.add("edit");
-  task_edit_element.innerHTML = "Edit";
-  task_actions_element.appendChild(task_edit_element);
-
-  task_element.appendChild(task_actions_element);
+  const task_input_element = task_element.querySelector(".text");
+  const task_success_element = task_element.querySelector(".success");
+  const task_delete_element = task_element.querySelector(".delete");
+  const task_edit_element = task_element.querySelector(".edit");
 
   addTaskActions(task_element, task_input_element, task_edit_element, task_success_element, task_delete_element);
 
